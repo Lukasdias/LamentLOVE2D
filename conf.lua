89cbbef3d8a7e7599c6b3ee59131fb907ac510a9
Lukas Henrique <zithofdias@gmail.com>
2018-07-09 03:18:07 -0300

    Main, Conf and map
    Map - Table with all information of my Tiled Map

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+    t.window.fullscreentype = "desktop"
+    t.modules.physics = true
+    t.window.width = 770
+    t.window.height = 580
+    t.window.title = "The Lament of Laurence"        
+    t.window.icon = "imagens/menu/game_title.png"
+end
---
+++ main.lua
@@ -0,0 +1,84 @@
+require("modules/laurence")
+require("modules/map_conf")
+require("modules/menu")
+require("modules/tutorial")
+require("modules/Ost")
+require("modules/npc")
+require("modules/points")
+require("modules/GameOver")
+require("modules/End")
+
+largura = love.graphics.getWidth()
+altura = love.graphics.getHeight()
+
+function love.keyreleased(key)
+	last_keyreleased(key)
+end
+
+--[[function love.keypressed(key)
+	if key == backspace then
+		if gamestate == "tutorial" then
+			gamestate = "title"
+			back = true
+		end
+	end
+end--]]
+
+
+function love.load()	
+	Map_load()
+	tutorial_load()
+	menu_load()
+	songs_load()
+	npc_load(map)
+	Laurence_load(map, world)
+	points_load(map, world)
+	gameover_load()
+	Ending_load()
+end
+
+function love.update(dt)
+	if gamestate == "title" then
+		menu_update(dt)
+	elseif gamestate == "play" then
+		Map_update(dt)
+		Laurence_update(dt)
+		points_update(dt)
+		npc_update(dt)
+		points_update(dt)
+	elseif gamestate == "tutorial" then 
+		tutorial_update(dt)
+	elseif gamestate == "Game Over" then
+		gameover_update(dt)
+	elseif gamestate == "Ending" then
+		Ending_update(dt)
+	end
+end
+
+function love.draw()
+  if gamestate == "title" then
+		menu_draw()
+  elseif gamestate == "tutorial" then
+		tutorial_draw()
+  elseif gamestate == "play" then
+		Map_draw()
+  elseif gamestate == "Game Over" then
+		gameover_draw()
+  elseif gamestate == "Ending" then
+	Ending_draw()
+  end
+end
+
+
+function love.mousemoved(x,y)
+    start_button.mousemoved(x,y)
+    quit_button.mousemoved(x,y)
+    help_button.mousemoved(x,y)
+end
+  
+function love.mousepressed(x,y,b,it)
+    start_button.mousepressed(x,y,b)
+    quit_button.mousepressed(x,y,b)
+    help_button.mousepressed(x,y,b)
+end
+
