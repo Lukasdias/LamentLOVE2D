925e7f25d69bad432de1b55b944a9799806f644b
Lukas Henrique <zithofdias@gmail.com>
2018-09-07 19:15:54 -0300

    Add files via upload

---
+++ map/End_Background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="End_Background" tilewidth="32" tileheight="32" tilecount="637" columns="49">
+ <image source="../../End_Background.gif" trans="740900" width="1578" height="432"/>
+</tileset>
